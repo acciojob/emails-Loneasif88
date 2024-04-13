7980af00d23c436b32457b26b33b471b784bc813
Loneasif88 <erasif88@gmail.com>
2024-04-13 11:33:59 +0530

    workplace error 4

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -30,56 +30,31 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
     	//Collections.sort(calendar);
-    	 ArrayList<Meeting> m = new ArrayList<>();
-         LocalTime time_limit ;
-         // Sorting of meeting according to
-         // their finish time.
+    	 ArrayList<Meeting> curr = new ArrayList<>(calendar);
 
-         // sorting using compartor class and without (compartor or comparable class)
-         //  mycomparator mc = new mycomparator();
-         //  Collections.sort(calendar,mc);
-         Collections.sort(calendar,(m1,m2)->{
-             return m1.getEndTime().compareTo(m2.getEndTime());
-         });
+         Collections.sort(curr, Comparator.comparing(Meeting::getStartTime));
 
-         // Initially select first meeting.
-         Meeting meet = calendar.get(0);
-         m.add(meet);
-         // time_limit to check whether new
-         // meeting can be conducted or not.
-         time_limit = calendar.get(0).getEndTime();
+         LocalTime prevS = curr.get(0).getStartTime();
+         LocalTime prevE = curr.get(0).getEndTime();
 
-         // Check for all meeting whether it
-         // can be selected or not.
-         for (int i = 1; i < calendar.size(); i++) {
-             if (calendar.get(i).getStartTime().compareTo(time_limit)>0) {
+         int count=0;
 
-                 // Add selected meeting to arraylist
-                 m.add(calendar.get(i));
+         for(int i=1;i<curr.size();i++){
 
-                 // Update time limit
-                 time_limit = calendar.get(i).getEndTime();
+             LocalTime currS = curr.get(i).getStartTime();
+             LocalTime currE = curr.get(i).getEndTime();
+
+             if(currS.isAfter(prevE)){
+                 count++;
+                 prevS = currS;
+                 prevE = currE;
+             }else{
+                 if(prevE.isAfter((currE))){
+                     prevS = currS;
+                     prevE = currE;
+                 }
              }
          }
-
-         return m.size();
-     }
-     class mycomparator implements Comparator<Meeting> {
-         @Override public int compare(Meeting m1, Meeting m2)
-         {
-             if (m1.getEndTime().compareTo(m2.getEndTime())<0) {
-
-                 // Return -1 if second object is
-                 // bigger than first
-                 return -1;
-             }
-             else if (m1.getEndTime().compareTo(m2.getEndTime())>0) {
-
-                 // Return 1 if second object is
-                 // smaller than first
-                 return 1;
-             }
-             return 0;
-         }
+         return count+1;
     }
 }
